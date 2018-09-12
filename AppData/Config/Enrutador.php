e9fd5b8a5ab1e071c81b26c040ac0675eb413e47
OscarB007 <oscarinbau@gmail.com>
2018-09-12 16:52:27 -0500

    Enrutador
    Se modifico el enrutador

--- AppData/Config/Enrutador.php
+++ AppData/Config/Enrutador.php
@@ -8,12 +8,43 @@ class Enrutador
 		$controlador = $request->getControlador() . "Controller";
 		$ruta = ROOT . "AppData" . DS ."Controller" . DS . $controlador . ".php";
 		$metodo = $request->getMetodo();
-		$argumrnto = $request->getArgumento();
+		$argumento = $request->getArgumento();
 
 		if(is_readable($ruta)) {
 			require_once($ruta);
 			$mostrar = "AppData\\Controller\\" . $controlador;
+			$controlador = new $mostrar;
+			if(!isset($argumento)){
+				$datos =call_user_func(array($controlador, $metodo));
+			}else{
+				$datos= call_user_func(array($controlador,$metodo, $argumento));
+			}
+
 			
 		}
+		if(request->getMetodo()!="modificar")
+		{
+			ruta = ROOT . "Views" . DS . $request->getControlador() . DS . $request->getMetodo . ".php"
+			if(is_readable($ruta))
+			{
+				require_once()
+			}
+			else
+				if($request->getMetodo()== "eliminar" || $request->
+					getMetodo()=="actualizar" || $request->getMetodo()== "crear")
+				{
+					$ruta = ROOT . "Views" . DS . $request->getControlador() . DS . "tabla" . ".php";
+					if(is_readable($ruta))
+					{
+						require_once($ruta);
+					}
+					else
+						echo "Esta pagina no existe";
+				}
+		}
+		else
+			echo(json_encode(mysql_fetch_assoc($datos));
+	 }
 	}
-}
+   }
+?>}
