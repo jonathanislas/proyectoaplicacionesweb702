7569034833c78c3a9c1f0099dc46c9c981b09405
jonathanislas31 <jonathanislas31@gmai.com>
2018-11-07 01:30:07 -0600

    cambios editar

--- Views/ver/index.php
+++ Views/ver/index.php
@@ -39,6 +39,36 @@
         
   </tbody>
 </table>
+<!-- <div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Editando</h4>
+        <button type="button" class="close" data-dismiss="modal">&times</button>
+      </div>
+      <div class="modal-body">
+        <form class="form-signin" action="" method="post">
+          <div class="form-group">
+            <input type="text" class="form-control" id="nombre" nombre="nombre"></input>
+            <label for="nombre">Nombre</label>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="app" nombre="ap_p"></input>
+            <label for="nombre">Apellido Paterno</label>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="apm" nombre="ap_m"></imput>
+            <label for="nombre">Apellido Materno</label>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-succes" data-dismiss="modal">Actualizar</button>
+      </div>
+    </div>
+  </div>
+</div> -->
+
 <div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -59,6 +89,7 @@
     </div>
   </div>
 </div>
+
 <script type="text/javascript">
   $(document).ready(function(){
     $(".editar").click(function(){
